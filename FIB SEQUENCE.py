aff766e487570b716e4c7cabd93147cb184c616c
beduopaulinus4 <beduopaulinus4@gmail.com>
2025-11-09 02:53:28 +0000

    Add files via upload
    Signed-off-by: beduopaulinus4 <beduopaulinus4@gmail.com>

---
+++ FIB SEQUENCE.py
@@ -0,0 +1,10 @@
+def fibonacci_loop(n: int):
+    """ Return the first n fibonacci numbers (starting with 0)"""
+    if n <= 0:  # type: ignore
+        return []
+    if n == 1:
+        return [0]
+    seq = [0, 1]
+    for i in range(2, n):
+        seq.append(seq[-1] + seq[-2])
+    return seq
